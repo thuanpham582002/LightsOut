4d3cf9a709b1feb59c89f444fe944349276995a8
AlonX2 <73618411+AlonX2@users.noreply.github.com>
2024-11-26 23:43:34 +0200

    Initial Commit

---
+++ LightsOut/LightsOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsOutApp.swift
+//  LightsOut
+//
+//  Created by Alon Natapov on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LightsOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LightsOutTests/LightsOutTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightsOutTests.swift
+//  LightsOutTests
+//
+//  Created by Alon Natapov on 26/11/2024.
+//
+
+import Testing
+@testable import LightsOut
+
+struct LightsOutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
