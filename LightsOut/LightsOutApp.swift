9ed0205813f149cef3eb0123e9892a44baca011c
AlonX2 <73618411+AlonX2@users.noreply.github.com>
2024-12-06 01:49:47 +0200

    Move to AppKit-based popover.

--- LightsOut/LightsOutApp.swift
+++ LightsOut/LightsOutApp.swift
@@ -3,16 +3,85 @@ import CoreGraphics
 import AppKit
 import Sparkle
 
+import SwiftUI
+import AppKit
+
 @main
 struct LightsOutApp: App {
-    @StateObject private var viewModel = DisplaysViewModel()
-    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        MenuBarExtra("LightsOut", image: "MenubarIcon") {
-            MenuBarView()
-                .environmentObject(viewModel)
+        Settings {
+            EmptyView()
         }
-        .menuBarExtraStyle(.window)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    var popover: NSPopover!
+    var eventMonitor: Any?
+    let displaysViewModel = DisplaysViewModel()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        popover = NSPopover()
+        popover.behavior = .applicationDefined
+
+        // Set up the status item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(named: "MenubarIcon")
+            button.action = #selector(handleClick(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] _ in
+            if self?.popover.isShown == true {
+                self?.popover.performClose(nil)
+            }
+        }
+    }
+
+    @objc func handleClick(_ sender: NSStatusBarButton) {
+        guard let event = NSApp.currentEvent else { return }
+        if event.type == .rightMouseUp {
+            showContextMenu()
+        } else {
+            togglePopover(sender)
+        }
+    }
+
+    func togglePopover(_ sender: NSStatusBarButton) {
+        if popover.isShown {
+            popover.performClose(sender)
+        } else {
+            // Reuse the existing view model instance
+            let contentView = MenuBarView().environmentObject(displaysViewModel)
+            popover.contentViewController = NSHostingController(rootView: contentView)
+
+            if let button = statusItem.button {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+                NSApp.activate(ignoringOtherApps: true)
+            }
+        }
+    }
+
+    func showContextMenu() {
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(
+            title: "Quit LightsOut",
+            action: #selector(NSApplication.terminate(_:)),
+            keyEquivalent: "q"
+        ))
+        statusItem.menu = menu
+        statusItem.button?.performClick(nil)
+        statusItem.menu = nil
+    }
+
+    deinit {
+        if let monitor = eventMonitor {
+            NSEvent.removeMonitor(monitor)
+        }
     }
 }
 
